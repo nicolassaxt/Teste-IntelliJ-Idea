acfc506328edc3ba2b9af39ee69bfdbf267f2781
Nicolas <nicolassaxt@gmail.com>
2021-10-12 19:55:24 -0300

    Utilizando commit no Intellij

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,12 +6,49 @@ public class PrimeiroPrograma {
 
     public static void main(String[] args) {
 
-        Gato gato = new Gato();
+        Gato gato = new Gato("Creca","Rajada","7");
 
         System.out.println(gato);
 
+        Livro livro = new Livro("A Game of Thrones (A Guerra dos Tronos)", 694);
+
+        System.out.println(livro);
      /*   int a = 5;
         int b = 3;
         System.out.println("Hello Wolrd! " + (a+b));*/
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
